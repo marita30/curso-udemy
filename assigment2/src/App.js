89685f233f5e9178dc9fbbc2d74d3c293486becf
marita30 <marita.jose.ch@gmail.com>
2020-02-24 15:05:48 -0300

    Add the station of the work

---
+++ assigment2/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+ class App extends Component {
+   render() {
+      return (
+        <div className="App">
+        <li> </li>
+       </div>
+      );
+    }
+  }
+
+export default App;
