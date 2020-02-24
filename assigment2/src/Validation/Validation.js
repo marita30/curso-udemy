3a768ea4e3c32325462ffb62c927746e22424ea1
marita30 <marita.jose.ch@gmail.com>
2020-02-24 17:37:32 -0300

    Agregando un ternario para cambiar el texto conforme al inputLength

--- assigment2/src/Validation/Validation.js
+++ assigment2/src/Validation/Validation.js
@@ -3,7 +3,14 @@ import React from 'react';
 const validation = (props) => {
     return(
         <div>
-            <p> Text too short </p>
+            {/* Ternario, si el metodo que esta en App.js inputLength es mayor que si poner el primer parrafo de lo contrario el otro. */}
+            
+            {
+                props.inputLength > 5 ?
+                <p> Text long enough </p>:
+                <p> Text too short </p>
+            }
+           
         
         </div>
     );
