e16e3ac2aaf4c12c259b5510d0d02f2702ee9197
marita30 <marita.jose.ch@gmail.com>
2020-02-24 16:35:22 -0300

    agregando las intrucciones de la tarea a realizar , tambien se agrego el state con una variable llamada userInput vacia, luego un metodo para cambiar el valor de esa variable llamado inputChangedHandler y un input con onChanged

--- assigment2/src/App.js
+++ assigment2/src/App.js
@@ -2,11 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 
  class App extends Component {
+  state = {
+    /* Declaras una variable llamada userInput */
+    userInput: " "
+  }
+   /* Cambia el valor de la variable userInput */
+  inputChangedHandler = (event) => {
+    this.setState({userInput: event.target.handler});
+  }
+
    render() {
       return (
+       
         <div className="App">
-        <li> </li>
+          <ol>
+            <li> create an input field (in App component) with a change listener which outputs the length of the entered text below it(e.g in a pharagraph)</li>
+            <li>Create a new component(=>ValidationComponent) which receives the text length as a prop</li>
+            <li>Inside the validationComponent, either output "Text long enough" depending on the text length (e.g. take 5 as a minimun length) </li>
+           <li> Create another component (=> CharComponent) and style it as an inline box(=> display: inline-block, padding: 16px, text-align: center, margin: 16px, border: 1px solid.</li>
+           <li>Render a list of CharComponent where each CharComponent receives a different letter of the entered text(in the initial input fiel) as a prop</li>
+           <li>When you click a CharComponent, it should be removed from the entered text.</li>
+         </ol>
+         <p>Hint: Keep in mind that Javascript strings are basically array!</p>
+         <hr />
+         <input type= "text" onChange={this.inputChangedHandler} />
        </div>
+       
       );
     }
   }
