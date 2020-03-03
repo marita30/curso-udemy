74c45be0b91e974632b53f7a7c7fd346556442b7
marita30 <marita.jose.ch@gmail.com>
2020-03-03 15:15:39 -0300

    Importando el archivo Layout y de igual manera poniendo una etiqueta de <Layout> envolviendo <p>

--- burger/src/App.js
+++ burger/src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Layout from './components/Layout/Layout'
 
-function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+class App extends Component{
+  render() {
+    return (
+      <div>
+        <Layout>
+          <p>Test</p>
+
+        </Layout>
+      
+      </div>
+    );
+  }
 }
 
 export default App;
