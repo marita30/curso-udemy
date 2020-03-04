e497011d31e42aa6c4a3b62627a4e44160adf189
marita30 <marita.jose.ch@gmail.com>
2020-03-04 14:02:24 -0300

    Creando mas comentarios al componete transformedIngredients..... primero usamos Object.keys(props.ingredients) que le decimos que pase todos esos elementos que tiene el state en un arreglo o matriz , lueho le agregamos un puntero llamado igKey para return un arreglo con la dimension del props.ingredients[igKey] que en este caso el igKey representa a la cantidad de cada uno de los ingredientes que va tener la hamburgues, luego volvemoa a mapiar y return la etiqueta <BurgeBuilder con una llave qe sea igKey = i que ahi igKey hace referencia al nombre del ingrediente y i hace referencia al subindice del arreglo

--- burger/src/components/Burger/Burger.js
+++ burger/src/components/Burger/Burger.js
@@ -4,15 +4,19 @@ import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
     /* Object tiene un metodo clave que extrae las claves de un objeto dado y las convierte en una matriz , por ejemplo en este 
-    caso covertira el state de los ingredientes en un string, luego lo mapeamos porque ya es una matriz para poder recorrerla, igKey para la clave de ingrediente*/
+    caso covertira el state de los ingredientes en un string, luego lo mapeamos porque ya es una matriz para poder recorrerla, igKey ES NUESTRO PUNTERO*/
+     /* Object.keys(props.ingredients) es para pasar los ingredientes de forma de un arreglo */
+     console.log(props.ingredients)
     const transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
         /* esparcir o crear una nueva  matriz con tres espacios libres (3) en este caso es el espacio que nos dara props.ingredients luego otra vez mapiamos los elementos del nuevo arrego
         usaremos (_) para indicar que el indice de ese elemento es importante para mi.*/
+        
         return[...Array(props.ingredients[igKey])].map((_, i) =>{
+            console.log(i)
            return <BurgerIngredient key={igKey + i} type= {igKey} />;/* Esto crea una llave unica para cada ingrediente. */
 
-        });
+        }); 
     });
     return(
        /*  Agregando los ingredientes del archivo BurgerIngredient pero no dinamicamente. */
