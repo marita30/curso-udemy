2a2bc6e921d919f275d9ea650bf99004a353a152
marita30 <marita.jose.ch@gmail.com>
2020-03-04 13:33:27 -0300

    Creamos un metodo generico de produccion de ingredientes, con Object

--- burger/src/components/Burger/Burger.js
+++ burger/src/components/Burger/Burger.js
@@ -3,12 +3,22 @@ import classes from './Burger.css';
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
+    /* Object tiene un metodo clave que extrae las claves de un objeto dado y las convierte en una matriz , por ejemplo en este 
+    caso covertira el state de los ingredientes en un string, luego lo mapeamos porque ya es una matriz para poder recorrerla, igKey para la clave de ingrediente*/
+    const transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+        /* esparcir o crear una nueva  matriz con tres espacios libres (3) en este caso es el espacio que nos dara props.ingredients luego otra vez mapiamos los elementos del nuevo arrego
+        usaremos (_) para indicar que el indice de ese elemento es importante para mi.*/
+        return[...Array(props.ingredients[igKey])].map((_, i) =>{
+           return <BurgerIngredient key={igKey + i} type= {igKey} />;/* Esto crea una llave unica para cada ingrediente. */
+
+        });
+    });
     return(
        /*  Agregando los ingredientes del archivo BurgerIngredient pero no dinamicamente. */
         <div className={classes.Burger}>
             <BurgerIngredient type= "bread-top"/>
-            <BurgerIngredient type= "cheese"/>
-            <BurgerIngredient type= "meat"/>
+            {transformedIngredients}
             <BurgerIngredient type= "bread-bottom"/>
 
         </div>
