692d4e8dbdfd08817c094876c99226118d7c727d
marita30 <marita.jose.ch@gmail.com>
2020-03-04 13:14:21 -0300

    agregando state para hacer dinamicamente los ingredientes de la hamburguesa

--- burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,11 +3,28 @@ import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+
+    /* Este es una forma de hacer los ingredientes dinamicamente */
+
+   /*  constructor(props){
+        super(props);
+        this.state = {...}
+    } */
+
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 2,
+            cheese: 2,
+            meat: 2
+        }
+
+    }
     render(){
 
         return(
             <Aux>
-                <Burger />
+                <Burger ingredients={this.state.ingredients}/>
                 <div>Build Controls</div>
             </Aux>
         );
