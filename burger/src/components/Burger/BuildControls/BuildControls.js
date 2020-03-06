2b63241de30c0611222ca27f99bc79b137b0ecfd
marita30 <marita.jose.ch@gmail.com>
2020-03-06 13:56:44 -0300

    agregando una matriz llamada controls y importamos el archivo BuildControl y classes del archivo .css , luego al div incorporamos las classes y mapeamos con el .map la matriz control asignandole un puntero llamado ctrl

--- burger/src/components/Burger/BuildControls/BuildControls.js
+++ burger/src/components/Burger/BuildControls/BuildControls.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
+
+/* Creando una matriz */
+
+const controls = [
+    {label: 'Salad', type: 'salad'},
+    {label: 'Bacon', type: 'bacon'},
+    {label: 'Cheese', type: 'scheese'},
+    {label: 'Meat', type: 'meat'},
+
+];
 
 const buildControls = (props) => (
-    <div>
+    <div className = {classes.BuildControls}>
+        {controls.map(ctrl =>(  /* Mapeamos la matriz llamada controls y luego ctrl que es el puntero del .map como el .each , luego a la etiqueta le asignamos el puntero ctrl.label que 
+        label es un identificador unico. */
+            <BuildControl key ={ctrl.label} label={ctrl.Label}/>
+        ))}
 
         
     </div>
