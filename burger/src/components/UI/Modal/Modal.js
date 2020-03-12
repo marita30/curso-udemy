3d606da23fcfd63696672d92f1f3640e3f3f57eb
marita30 <marita.jose.ch@gmail.com>
2020-03-12 16:09:53 -0300

    agregamos a la etiqueta Backdrop un evento llamado clicked que tendra props.modalClosed que estara en el archivo BurgerBuilder.js que es para cerrar el telon del fondo

--- burger/src/components/UI/Modal/Modal.js
+++ burger/src/components/UI/Modal/Modal.js
@@ -5,7 +5,7 @@ import Backdrop from './Backdrop/Backdrop';
 
 const modal = (props) => (
     <Aux>
-        <Backdrop show={props.show}/> {/* para agregar el telon de fondo */}
+        <Backdrop show={props.show} clicked={props.modalClosed}/> {/* para agregar el telon de fondo */} {/* clicked viene del archivo modal.js */}
         <div className={classes.Modal}
             style={{
                 
