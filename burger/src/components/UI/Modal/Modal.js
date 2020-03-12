436f7869bd5c2cd5bac9dc191c4c6af7bf700b2a
marita30 <marita.jose.ch@gmail.com>
2020-03-12 15:21:06 -0300

    agregamos una variable style , con estilos de css para la variable show que viene del archivo BurgerBuilder.js

--- burger/src/components/UI/Modal/Modal.js
+++ burger/src/components/UI/Modal/Modal.js
@@ -2,7 +2,13 @@ import React from 'react';
 import classes from './Modal.css';
 
 const modal = (props) => (
-    <div className={classes.Modal}>
+    <div className={classes.Modal}
+        style={{
+            
+            transform: props.show ?  'translateY(0)' : 'translateY(-100vh)',/* transformacion si Show es verdadero, si es tru translateY
+            (0) y si no el otro.  */
+            opacity: props.show ? '1' : '0'
+        }}>
         {props.children}
 
     </div>
