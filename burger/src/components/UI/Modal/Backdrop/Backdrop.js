a80187ddbd5751a7d029a8d0980cddf083f4b213
marita30 <marita.jose.ch@gmail.com>
2020-03-12 16:10:58 -0300

    le agregamos el evento onClick con el props.clicked

--- burger/src/components/UI/Modal/Backdrop/Backdrop.js
+++ burger/src/components/UI/Modal/Backdrop/Backdrop.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import classes from './Backdrop.css';
-import { findNodeModule } from 'jest-resolve';
 
 /* Para el telon de findNodeModule. */
 
 const backdrop = (props) => (
 
-    props.show ? <div className={classes.Backdrop}>
+    props.show ? <div className={classes.Backdrop} onClick={props.clicked}> 
 
     </div> : null /* Verificr si es verdadero o falso */
 );
