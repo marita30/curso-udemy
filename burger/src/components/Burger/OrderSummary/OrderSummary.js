568e5d3a6eed928911243064b6821d8b11d86eec
marita30 <marita.jose.ch@gmail.com>
2020-03-17 11:36:23 -0300

    Agregando al total price .toFixed(2) para que muestre solo dos decimales

--- burger/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger/src/components/Burger/OrderSummary/OrderSummary.js
@@ -18,7 +18,7 @@ const orderSummary = (props) => {
             <ul> 
                 {ingredientSummary}
             </ul>
-            <p><strong>Total Price: {props.price} </strong></p>
+            <p><strong>Total Price: {props.price.toFixed(2)} </strong></p>
             <p>Continue to Checkout?</p>
             <Button btnType="Danger" clicked={props.purchaseCancelled}> CANCEL</Button> {/* btnType viene del archivo Button.js  y Clicked del archivo BurgerBuilder.js*/} 
             <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
