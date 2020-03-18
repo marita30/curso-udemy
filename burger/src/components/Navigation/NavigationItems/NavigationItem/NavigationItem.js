ec6cb8c66cef737b854bafbc7e2404873eea8fe7
marita30 <marita.jose.ch@gmail.com>
2020-03-18 11:42:24 -0300

    Agregando nueva carperta lllamada NavigationItems y NavigationItem

---
+++ burger/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -0,0 +1,10 @@
+ import React from 'react';
+import navigationItems from '../NavigationItems';
+
+ const navigationItem = (props) => (
+     
+    <li><a href="/">A link</a></li>
+
+ );
+
+ export default navigationItem;
