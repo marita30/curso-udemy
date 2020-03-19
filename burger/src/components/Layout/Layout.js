8956e66301aa541df80897030d9e19a8036d9e56
marita30 <marita.jose.ch@gmail.com>
2020-03-19 13:31:04 -0300

    Layout paso hacer una class para poder implementar el metodo de hacer click y que desaparezca el telon , elegimos este archivo porque tiene los dos metodos principales que es Toolbar y SideDrawer , una vez cambiando a clase inicializamos un state con una variable llamada showSideDrwaer:true y luego el metodo para cerrar el telon llamada sideDrawerClosedHnadler que se le pondra un estado de la variable state a false luego en la etiqueta <SideDrawer pasaremos open={this.state.showSideDrawer} y closed={this.sideDrawerClosedHnadler} que es el metodo para cerrar

--- burger/src/components/Layout/Layout.js
+++ burger/src/components/Layout/Layout.js
@@ -1,23 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar'
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = ( props ) => (
-    <Aux>
-       <Toolbar/>
-       <SideDrawer />
+/* vamos a convertir Layout en class donde podemos implementar el metodo para que podamos escuc har ambas Partes
+o cerrar haciendo click en el fondo (telon) , elegimos aqui porque tiene a los dos el Toolbar y SideDrawer*/
+class Layout extends Component {
 
-        <main className= {classes.Content}> 
-            {props.children}
-        </main>
-    </Aux>
+    state={
+        showSideDrawer: true
+    }
+    /* METODO PARA CERRAR EL SIDEDRAWER */
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false})
 
-
-);
-
-
+    }
 
 
-export default layout;
+    render () {
+        return(
+            <Aux>
+
+                <Toolbar/>
+                <SideDrawer 
+                    open={this.state.showSideDrawer} 
+                    closed={this.sideDrawerClosedHandler}
+                />
+
+                <main className= {classes.Content}> 
+                    {this.props.children}
+                </main>
+
+            </Aux>
+        )
+    }
+} 
+    
+
+
+
+
+export default Layout;
