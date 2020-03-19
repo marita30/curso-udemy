4bd3fc99e65f05b697b1fa856f3ae58007312f44
marita30 <marita.jose.ch@gmail.com>
2020-03-19 12:58:26 -0300

    Agregaremos un telon de fondo, para eso importaremos el archivo Backdrop.js y Aux.j luego lo return primero envolvemos todo con la etiqueta <Aux> y luego ponemos la etiqueta <Backdrop show /> que show viene del archivo Backdrop de un props.show

--- burger/src/components/Navigation/SideDrawer/SideDrawer.js
+++ burger/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,17 +2,27 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux';
 
 const sideDrawer = (props) => {
     return(
-        <div className={classes.SideDrawer}> 
-            <div className={classes.Logo}>
-                <Logo />
+        < Aux >
+            <Backdrop show/> {/* viene del archivo Backdrop.js y el show es de la condicion que si props.show es verdadero que se active una clase si no que retorne null es para el telon.*/}
+            <div className={classes.SideDrawer}> 
+
+                <div className={classes.Logo}>
+
+                    <Logo />
+
+                </div>
+
+                <nav>
+                    <NavigationItems />
+                </nav>
+
             </div>
-            <nav>
-                <NavigationItems />
-            </nav>
-        </div>
+        </Aux>
     );
 
 }; /* Componente funcional normal en el que obtenemos algo y devolvemos algo. */
