3c5e9215d1b3de50f72fae506e64f81f05c24965
marita30 <marita.jose.ch@gmail.com>
2020-03-19 16:24:00 -0300

    Creando DrawerToggle.js , con una etiqueta <div con un evento onClick con props.clicked que viene del archivo Toolbar.js (clicked) es un  button llamado MENU

---
+++ burger/src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const drawerToggle = (props) => (
+    <div onClick={props.clicked}>MENU</div> /* props.clicked viene del archivo Toolbar.js */
+
+);
+
+
+export default drawerToggle;
