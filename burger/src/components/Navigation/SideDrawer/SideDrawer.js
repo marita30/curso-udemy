767a3b7f188eed915ae3e534a2c5e65757917140
marita30 <marita.jose.ch@gmail.com>
2020-03-19 15:40:11 -0300

    Agregando una nuava variable let porque se cambiara el valor llamada attachedClasses que tendra las classes SideDrwaer y Close del archivo SideDrawer.js , luego una condicion (if) diciendo que if props.open es verdad pasar la variable que declaramos anteriorente let attachedClasses y solo cambiamos la ultima classes que seria Open en ves de Close y luego se lo pasamos a la etiqueta <div className = {attachedClasses.join(' ')} se lo pasamos como un string

--- burger/src/components/Navigation/SideDrawer/SideDrawer.js
+++ burger/src/components/Navigation/SideDrawer/SideDrawer.js
@@ -6,10 +6,20 @@ import Backdrop from '../../UI/Backdrop/Backdrop';
 import Aux from '../../../hoc/Aux';
 
 const sideDrawer = (props) => {
+
+    let  attachedClasses = [classes.SideDrawer, classes.Close]; /* classes.Close y classes.SideDrawer viene del archivo SideDrawer.css */
+    if (props.open) {
+        attachedClasses = [classes.SideDrawer, classes.Open];
+
+    }
     return(
         < Aux >
-            <Backdrop show={props.open} clicked={props.closed}/> {/* /* viene del archivo Backdrop.js y el show es de la condicion que si props.show es verdadero que se active una clase si no que retorne null es para el telon.*/}
-            <div className={classes.SideDrawer}>  {/* /* closed viene del archivo Layout.js del metodo  sideDrawerClosedHandler. de igual forma props.open */}
+            <Backdrop 
+                show={props.open} 
+                clicked={props.closed}
+            /> {/* /* viene del archivo Backdrop.js y el show es de la condicion que si props.show es verdadero que se active una clase si no que retorne null es para el telon.*/}
+            
+            <div className={attachedClasses.join(' ')}>  {/* /* closed viene del archivo Layout.js del metodo  sideDrawerClosedHandler. de igual forma props.open */}
 
                 <div className={classes.Logo}>
 
