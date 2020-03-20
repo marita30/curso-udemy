ab6f4f5af6fe9028ff60645120b65c2d3d6f6cd9
marita30 <marita.jose.ch@gmail.com>
2020-03-20 14:13:36 -0300

    Importando classes del archivo css DrawerToggle y pasandoselo a la etiqueta <div> donde se encentra el evento onClick

--- burger/src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ burger/src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import classes from './DrawerToggle.css';
 
 const drawerToggle = (props) => (
-    <div onClick={props.clicked}>MENU</div> /* props.clicked viene del archivo Toolbar.js */
+    <div className={classes.DrawerToggle} onClick={props.clicked}>
+        <div></div>
+        <div></div>
+        <div></div>
+    </div> /* props.clicked viene del archivo Toolbar.js */
 
 );
 
