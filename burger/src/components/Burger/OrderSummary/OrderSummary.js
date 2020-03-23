67d940a05b4d8a77aad33fdbb1063aec3ec9e2c3
marita30 <marita.jose.ch@gmail.com>
2020-03-23 12:51:33 -0300

    Cambiando por class y agregando metodos para ver donde se actualiza la aplicacion por medio del console.log

--- burger/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,29 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Aux from '../../../hoc/Aux';
 import Button from '../../UI/Button/Button'
 
-const orderSummary = (props) => {
+class OrderSummary extends Component {
+    /* Metodo para ver cuando esto realmente se actualiza */
+    componentWillUpdate(){
+        /* se va actualizar cada que le damos click a cualquier boton que tenga la aplicacion. */
+        console.log('[OrderSummary] WillUpdate');
+    }
 
-    const ingredientSummary = Object.keys(props.ingredients)
+
+
+    render (){
+
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map(igKey => {
             return (
                 <li key={igKey}> 
-                    <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+                    <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
                 </li>);
         }   );
-    return (
-        <Aux>
-            <h3>Your Order</h3>
-            <p>A delicious burger with the fallowing ingredients:</p>
-            <ul> 
-                {ingredientSummary}
-            </ul>
-            <p><strong>Total Price: {props.price.toFixed(2)} </strong></p>
-            <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCancelled}> CANCEL</Button> {/* btnType viene del archivo Button.js  y Clicked del archivo BurgerBuilder.js*/} 
-            <Button btnType="Success" clicked={props.purchaseContinued}>CONTINUE</Button>
-        </Aux>
-    )    
-};
 
-export default orderSummary;         
+        return(
+
+            <Aux>
+                <h3>Your Order</h3>
+                <p>A delicious burger with the fallowing ingredients:</p>
+                <ul> 
+                    {ingredientSummary}
+                </ul>
+                <p><strong>Total Price: {this.props.price.toFixed(2)} </strong></p>
+                <p>Continue to Checkout?</p>
+                <Button btnType="Danger" clicked={this.props.purchaseCancelled}> CANCEL</Button> {/* btnType viene del archivo Button.js  y Clicked del archivo BurgerBuilder.js*/} 
+                <Button btnType="Success" clicked={this.props.purchaseContinued}>CONTINUE</Button>
+           </Aux>
+
+        );
+    }
+}
+
+export default OrderSummary;         
--- burger/src/components/UI/Modal/Modal.js
+++ burger/src/components/UI/Modal/Modal.js
@@ -1,23 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Modal.css';
 import Aux from '../../../hoc/Aux';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-    <Aux>
-        <Backdrop show={props.show} clicked={props.modalClosed}/> {/* para agregar el telon de fondo */} {/* clicked viene del archivo modal.js y modalClosed del archivo BurgerBuilder.js*/}
-        <div className={classes.Modal}
-            style={{
-                
-                transform: props.show ?  'translateY(0)' : 'translateY(-100vh)',/* transformacion si Show es verdadero, si es tru translateY
-                (0) y si no el otro.  */
-                opacity: props.show ? '1' : '0'
-            }}>
-            {props.children}
+class Modal extends Component {
 
-        </div>
-    </Aux>
-);
+    /* Creamos un componente de que solo se actualice si se muestran cambios. */
+    shouldComponentUpdate(nextProps, nextState){
+            return nextProps.show !== this.props.show;
+        } /* esto quiere decir que si nextProps.show si eso es igual a this.props.show entonces para el estado anterior quiero devolver true */
+    
+    /* Ahora creamos otro componente para confirmar si funciona */ /* Esto es para que se actualice solo cuando le demos al boton OrderNow  */
+    componentWillUpdate () {
+        console.log('[Modal] WillUpdate');
+    }
 
+    render (){
+        return(
+            <Aux>
+                <Backdrop show={this.props.show} clicked={this.props.modalClosed}/> {/* para agregar el telon de fondo */} {/* clicked viene del archivo modal.js y modalClosed del archivo BurgerBuilder.js*/}
+                <div className={classes.Modal}
+                    style={{
+                        
+                        transform: this.props.show ?  'translateY(0)' : 'translateY(-100vh)',/* transformacion si Show es verdadero, si es tru translateY
+                        (0) y si no el otro.  */
+                        opacity: this.props.show ? '1' : '0'
+                    }}>
 
-export default modal;
+                    {this.props.children}
+
+                </div>
+            </Aux>
+        );
+    }
+}
+export default Modal;
