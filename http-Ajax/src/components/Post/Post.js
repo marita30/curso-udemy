9180a655db3e566621db038eae32f4a6fb0afcf4
marita30 <marita.jose.ch@gmail.com>
2020-03-24 17:10:28 -0300

    agregandole el evento onClick en la etiqueta <article> con el props.clicked que viene del archivo blog.js es para poder darle click a cada uno de los posts indivudialmente

--- http-Ajax/src/components/Post/Post.js
+++ http-Ajax/src/components/Post/Post.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './Post.css';
 
 const post = (props) => (
-    <article className="Post">
+    <article className="Post" onClick={props.clicked}> {/* viene del archivo Bog.js */}
         <h1>{props.title}</h1> {/* el props.title viene del archivo Blog.js */}
         <div className="Info">
             <div className="Author">{props.author}</div> {/* viene del archivo Bog.js */}
