1cede9982da88465b41c07ed09734d7b4dfed31a
marita30 <marita.jose.ch@gmail.com>
2020-03-24 17:12:17 -0300

    agregando una condicion , if this.props.id es false o null no entrada a imprimiro , pero si es true lo iimprimira y le agregmos style a la etiqueta <p> donde tenemo please select a post para que el texto se muestre al centro

--- http-Ajax/src/components/FullPost/FullPost.js
+++ http-Ajax/src/components/FullPost/FullPost.js
@@ -4,17 +4,22 @@ import './FullPost.css';
 
 class FullPost extends Component {
     render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
+        let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
+        if (this.props.id) {  /* si this.props.id es false o null no entra al bloque pero si es true entra a este bloque. viene del archivo blog.js*/
 
-        );
+            post = (
+                <div className="FullPost">
+                    <h1>Title</h1>
+                    <p>Content</p>
+                    <div className="Edit">
+                        <button className="Delete">Delete</button>
+                    </div>
+                </div>
+    
+            );
+
+        }
+        
         return post;
     }
 }
