d9e4b547d58d274cb312b2e25e6fab19407b5bbf
marita30 <marita.jose.ch@gmail.com>
2020-03-24 14:31:53 -0300

    en la etiqueta <h1> le agregamos {props.title} que lo delcaramos en el archivo blog.js

--- http-Ajax/src/components/Post/Post.js
+++ http-Ajax/src/components/Post/Post.js
@@ -4,7 +4,7 @@ import './Post.css';
 
 const post = (props) => (
     <article className="Post">
-        <h1>Title</h1>
+        <h1>{props.title}</h1> {/* el props.title viene del archivo Blog.js */}
         <div className="Info">
             <div className="Author">Author</div>
         </div>
