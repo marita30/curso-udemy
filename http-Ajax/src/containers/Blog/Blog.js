05bc7ccd7002b65faca6bdc75405e3ad50a590f3
marita30 <marita.jose.ch@gmail.com>
2020-03-24 14:30:21 -0300

    Importando axios , declarando un state con una variable llamada posts: [] inicializada como un string vacio, luego, creamo un metodo llamado componentDidMount y dentro de ese metodo le hacemos una peticion a la api y luego le celaramos un estado this.setState con la variable posts: response.data le decimos que el string que esta en el state ahora tenga toda la data que contiene la url de la api, luego en el render creamos otra variable llamada posts y le pasamos la variable del state y lo mapeamos , le agregamos un puntero que se llama post y por ultimo se lo pasamos a la etiqueta <post> con una variable llamada title = {post.title}

--- http-Ajax/src/containers/Blog/Blog.js
+++ http-Ajax/src/containers/Blog/Blog.js
@@ -8,19 +8,29 @@ import './Blog.css';
 
 class Blog extends Component {
 
+    state = {
+        posts: []
+
+    }
+
   /*   Le decimos al que una vez que se carge este contenedor muestre la informacion en la direccion que le declaramos axios.get */
     componentDidMount (){
 
         axios.get('http://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                this.setState({posts: response.data}); /* delcaramos un estado que le decimos que la variable declarada en el state que se llama posts: contenga la informacion de data que seria el arreglo con los hashes que nos da la url de axios.get */
+                //console.log(response);
+            }); /* esto es igual a decir const respose = axios.get('http://jsonplaceholder.typicode.com/posts') y luego console.log(response) y luego vas a verificar a la console la informacion que trae de la api. */
 
     }
-    render () {
+    render () { 
+        const posts = this.state.posts.map( post => { 
+            return <Post title = {post.title}/>     
+        });
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts} {/* viene del const posts. */}
                 </section>
                 <section>
                     <FullPost />
