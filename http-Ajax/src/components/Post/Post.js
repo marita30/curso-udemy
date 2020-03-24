9aeec9286076dfeb3b8978b97b61165e12cdf393
marita30 <marita.jose.ch@gmail.com>
2020-03-24 16:32:33 -0300

    le pasamos a la etiqueta <div> que tiene como className Author el props.author que viene del archivo blog.js que fue el que agregamos a la data, trasnformando la data

--- http-Ajax/src/components/Post/Post.js
+++ http-Ajax/src/components/Post/Post.js
@@ -6,7 +6,7 @@ const post = (props) => (
     <article className="Post">
         <h1>{props.title}</h1> {/* el props.title viene del archivo Blog.js */}
         <div className="Info">
-            <div className="Author">Author</div>
+            <div className="Author">{props.author}</div>
         </div>
     </article>
 );
