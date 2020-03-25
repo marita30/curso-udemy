d69094a6084d989831c079b2b4464e41c4e437ea
marita30 <marita.jose.ch@gmail.com>
2020-03-25 13:45:59 -0300

    dandole la funcionalidad al botton de add post , para crear nuevos post, primero importamos axios, luego creamos un metodo llamdo postDataHnadler que tendra una peticion axios.post a la url de la api y se la pasa como parametro dat que viene de una const que es un objecto como estara estructura el nuevo post y luego obtenemos un response y al final le agregamos a la etiqueta <button el evento onClick y le pasamos como parametro el metodo creado postDataHandler

--- http-Ajax/src/components/NewPost/NewPost.js
+++ http-Ajax/src/components/NewPost/NewPost.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './NewPost.css';
 
@@ -6,7 +7,20 @@ class NewPost extends Component {
     state = {
         title: '',
         content: '',
-        author: 'Max'
+        author: 'MJ'
+    }
+    //Para el botton de add Post. Para poder agregar un nuevo post
+    postDataHandler = () => {
+        const data = {
+            title: this.state.title,
+            body: this.state.content,
+            author: this.state.author
+        } // esto es para decir que queremos que nos imprima estos valores. y luego se lo pasamos la const como parametro a la url que hacemos la peticion a la api.
+        axios.post('https://jsonplaceholder.typicode.com/posts', data)
+        .then(response => {
+            console.log(response);
+        }); //POST
+
     }
 
     render () {
@@ -19,10 +33,10 @@ class NewPost extends Component {
                 <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
                 <label>Author</label>
                 <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
-                    <option value="Max">Max</option>
-                    <option value="Manu">Manu</option>
+                    <option value="MJ">M.J</option>
+                    <option value="CRIS">CRIS </option>
                 </select>
-                <button>Add Post</button>
+                <button onClick={this.postDataHandler}>Add Post</button>
             </div>
         );
     }
