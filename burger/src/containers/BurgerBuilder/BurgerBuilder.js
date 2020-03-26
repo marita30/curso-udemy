61944b17ae2b9f72beff9956abe66b527b3208eb
marita30 <marita.jose.ch@gmail.com>
2020-03-26 17:02:54 -0300

    Importamos el archivo spinner.js luego lo agregamos en el state y lo delcaramos false con la variable loading: false, luego en el render declaramos una let llamada orderSummary que va a contener todo lo que esta en la etiqueta <OrderSummary> luego le ponemos una condicion que if this.state.loading es true le pasamos la let orderSummary y le decimos que va ser = a < Spinner /> y luego en el return debajo de la etiqueta <Modal> le pasamos la variable orderSummary

--- burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,8 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+
+import Spinner from '../../components/UI/Spinner/Spinner';
 import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
@@ -35,7 +37,8 @@ class BurgerBuilder extends Component {
 
         totalPrice: 4, /* precio base */
         purchasable: false ,/* se convierte en verdad cuando podemos comprar una hamburguesa osea que la ahmburguesa tenga al menos un ingrediente. */
-        purchasing: false /* para saber si se hizo click en el boton OrderNow */
+        purchasing: false ,/* para saber si se hizo click en el boton OrderNow */
+        loading: false //spinner
     
 
     }
@@ -121,6 +124,7 @@ class BurgerBuilder extends Component {
     purchaseContinueHandler = () => {
         //alert('You continue!');
         //Declaramos la const order para crear una estructura con la informacion que tendra en la base de datos para las ordenes.
+        this.setState( {loading: true} );
         const order = {
             ingredients: this.state.ingredients,
             price: this.state.totalPrice,
@@ -139,8 +143,14 @@ class BurgerBuilder extends Component {
             deliveryMethod: 'fastest'
         }
         axios.post('/orders.json', order)//para agregarles las ordenes a firebase, le pasamos como parametros la const order.
-        .then(response => console.log(response))
-        .catch(error => console.log(error));
+        .then(response => {
+            this.setState({loading: false, purchasing: false});
+
+        })
+        .catch(error => {
+            this.setState({loading: false, purchasing: false});
+
+        });
     }
 
 
@@ -153,17 +163,25 @@ class BurgerBuilder extends Component {
 
             disableInfo[Key] = disableInfo[Key] <= 0  /* Key es el puntero que contiene la informacion de los ingredientes de la hamburguesa. */
         }
+
+        //para el spinner
+        let orderSummary =   < OrderSummary  
+            ingredients={this.state.ingredients} /* viene del archivo orderSummary.js */
+            price={this.state.totalPrice}
+            purchaseCancelled = {this.purchaseCancelHandler}
+            purchaseContinued = {this.purchaseContinueHandler}/>; 
+
+        if(this.state.loading) {
+            orderSummary = < Spinner />;
+
+        }
             
             
 
         return(
             <Aux>
                 <Modal show={this.state.purchasing}  modalClosed ={this.purchaseCancelHandler}>
-                    < OrderSummary  
-                        ingredients={this.state.ingredients} /* viene del archivo orderSummary.js */
-                        price={this.state.totalPrice}
-                        purchaseCancelled = {this.purchaseCancelHandler}
-                        purchaseContinued = {this.purchaseContinueHandler}/> 
+                  {orderSummary}
                         
                 </Modal>
                 <Burger ingredients={this.state.ingredients}/>
