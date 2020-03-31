17067d35e5cd3d9804f363e82eafb5ef4eec460c
marita30 <marita.jose.ch@gmail.com>
2020-03-31 13:41:18 -0300

    Agregando funcionalidad para la configuracion del error global de la aplicacion , con axios.interceptors

---
+++ burger/src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,49 @@
+import React, { Component }from 'react';
+import Modal from '../../components/UI/Modal/Modal';
+import Aux from '../Aux/Aux';
+
+const withErrorHandler = (WrappedComponent, axios) => {                 //que reciba como entrada WrappedComponent y como salida lo que tiene props.
+    return  class extends Component {
+
+        state = {
+
+            error: null
+        }
+
+        // gancho de ciclo de vida.
+        componentDidMount () {
+            axios.interceptors.request.use(req => {
+                this.setState({error: null});
+                return req;
+
+            }); //Interceptor para una solicitud... borrar cualquier error.
+            axios.interceptors.response.use(res => res, error => {
+                this.setState({error: error}); // el primer error es de state y el egundo es del puntero.
+
+            }); //Interceptor global para el error
+
+        }
+        // clicked.
+        errorConfirmedHandler = () => {
+
+            this.setState({error: null})
+        }
+
+        render () {
+            return (
+                < Aux >
+                    <Modal 
+                        show={this.state.error}
+                        modalClosed={this.errorConfirmedHandler}>  
+                        {this.state.error ? this.state.error.message : null} // si this.state.error es nulo, entonces generare el message de lo contrario generare nulo.
+                    </Modal>
+                    < WrappedComponent {...this.props} />                {/* distribuir cualquier accesorios que WrapedComponent pueda recibir */}
+                  
+                </Aux>
+            );
+        }
+    }
+}
+
+
+export default withErrorHandler;
