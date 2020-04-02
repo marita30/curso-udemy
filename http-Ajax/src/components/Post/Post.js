a5077973364dffb26f9c50f93094610dd10b0cee
marita30 <marita.jose.ch@gmail.com>
2020-04-02 15:42:57 -0300

    estamos configurando withRouter  que es para que los props se publiquen para las publicaciones o posts individuales

--- http-Ajax/src/components/Post/Post.js
+++ http-Ajax/src/components/Post/Post.js
@@ -1,14 +1,19 @@
 import React from 'react';
+//Es para que los props se publiquen para las publicaciones individuales , tienen una ubicacion de historial y una propiedad de coincidencia.
+import { withRouter } from 'react-router-dom';
 
 import './Post.css';
 
-const post = (props) => (
-    <article className="Post" onClick={props.clicked}> {/* viene del archivo Bog.js */}
-        <h1>{props.title}</h1> {/* el props.title viene del archivo Blog.js */}
-        <div className="Info">
-            <div className="Author">{props.author}</div> {/* viene del archivo Bog.js */}
-        </div>
-    </article>
-);
+const post = (props) => {
+    console.log(props)
+    return (
+        <article className="Post" onClick={props.clicked}> {/* viene del archivo Bog.js */}
+            <h1>{props.title}</h1> {/* el props.title viene del archivo Blog.js */}
+            <div className="Info">
+                <div className="Author">{props.author}</div> {/* viene del archivo Bog.js */}
+            </div>
+        </article>
+    );
+};
 
-export default post;
+export default withRouter(post);
