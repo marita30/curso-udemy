6e138324d56e8bedb9a2605312ae7b2abef43a5b
marita30 <marita.jose.ch@gmail.com>
2020-04-02 14:20:24 -0300

    Cambiando href por Link to para que las paginas no den refresh ejemplo: href=/ y <Link to=/

--- http-Ajax/src/containers/Blog/Blog.js
+++ http-Ajax/src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 //import axios from 'axios';  
-import { Route } from 'react-router-dom';
+import { Route, Link } from 'react-router-dom';
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
@@ -14,8 +14,12 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href="/">HOME</a></li>
-                            <li><a href="/new-post">NEW POST</a></li>
+                            <li><Link to="/">HOME</Link></li>
+                            <li><Link to={{
+                                pathname: '/new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>NEW POST</Link></li>
                         </ul>
                     </nav>
                 </header>
