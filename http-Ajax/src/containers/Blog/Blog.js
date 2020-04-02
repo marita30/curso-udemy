bbcbde290713572544f28c0b2495848e05c43a39
marita30 <marita.jose.ch@gmail.com>
2020-04-02 13:06:54 -0300

    renderizar ahora un componente, comentariamos la etiqueta <Route y volvemo agregar otra pero no con el render si no que ahora un component={Props} esto uiere decir que ahora vas a renderizar un componente que ontenga todos os Posts , ahora vamos a poder visualizar en la '/' home todos los posts en vez del <h1> que teniamos en el commit anterior.

--- http-Ajax/src/containers/Blog/Blog.js
+++ http-Ajax/src/containers/Blog/Blog.js
@@ -18,7 +18,8 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Route  path="/" exact render={() => <h1>HOME</h1>}/> {/* exact es un props booleano, tan solo agregarlo asi lo establece como verdadero y predeterminado es falso. */}
+                {/* <Route  path="/" exact render={() => <h1>HOME</h1>}/> */} {/* exact es un props booleano, tan solo agregarlo asi lo establece como verdadero y predeterminado es falso. */}
+                <Route path="/" exact  component={Posts}/> {/* Ya no vamos a  usar render, ahora usaremos component que deberia representarse en el lugar de esta ruta. POr ejemplo aqui le decimos que muestre todos los Posts. */}
             </div>
         );
     }
