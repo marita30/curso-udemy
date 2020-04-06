0c68e1ee4f9bd40aeb383bb07b372c0ed59746a4
marita30 <marita.jose.ch@gmail.com>
2020-04-06 13:02:58 -0400

    Borramos la configuracion que hicimos nterormente para que no se publique los post en la ruta new-post , ahora hacemos otra opcion para hacerlo, en el archivo Blog.js agregams a opcion Swicth en la import from react-router-dom y luego envolvemos con la etiqueta <Switch todas la route que tenemos en este archivo , lo que hacemos es que Switch solo va a cragar una de las rutas osea donde le di cick el usuario solo esa ruta va a cargar

--- http-Ajax/src/containers/Blog/Blog.js
+++ http-Ajax/src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 //import axios from 'axios';  
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, Switch} from 'react-router-dom';
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
@@ -33,9 +33,12 @@ class Blog extends Component {
                     </nav>
                 </header>
                 {/* <Route  path="/" exact render={() => <h1>HOME</h1>}/> */} {/* exact es un props booleano, tan solo agregarlo asi lo establece como verdadero y predeterminado es falso. */}
-                <Route path="/" exact  component={Posts}/> {/* Ya no vamos a  usar render, ahora usaremos component que deberia representarse en el lugar de esta ruta. POr ejemplo aqui le decimos que muestre todos los Posts. */}
-                <Route path="/new-post" component={NewPost}/>
-                <Route path="/posts/:id" component={FullPost}/>
+                <Switch>
+                    <Route path="/" exact  component={Posts}/> {/* Ya no vamos a  usar render, ahora usaremos component que deberia representarse en el lugar de esta ruta. POr ejemplo aqui le decimos que muestre todos los Posts. */}
+                    <Route path="/new-post" component={NewPost}/>
+                    <Route path="/:id" component={FullPost}/>
+                </Switch>  {/* //Solo carga una de las rutas  */}
+     
             </div>
         );
     }
