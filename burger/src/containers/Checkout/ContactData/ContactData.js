c0b8c0d3fbb010c898356033fdb3de45409df2df
marita30 <marita.jose.ch@gmail.com>
2020-04-13 15:57:17 -0400

    cREAMOS UN METODO LLAMADO ORDERHANDLER QUE TENDRA TODO EL CODIGO QUE TENIMOS EN BURGERBUILDER EN EL METODO PURCHASECONTINUEDHANDLER PARA METER EL PEDIDO EN LA BASE DE DATOS, CAMBIAMOS EL STATE POR PROPS.INGREDIENTS Y PROPS.PRICE, Y DECLARAMOS LOADING AL STATE COMO FALSE, LUEGO LE AGREGAMOS DEBJO DEL RESPONSE THIS.PROPS,HISTORY.PUH('/') QUE LE DECIMOS QUE UNA VEZ QUE EL CLIENTE LE DE CLICK AL BOTON ORDER DE LA RUTA /CHECKUT/CONTACTDATA SE REDIRECCIONE A LA PAGINA HOME, Y POR ULTIMO IMPORTAMOS EL SPINNER , Y LUEGO DEBAJO DEL RENDER DECLARAMOS UNA LET LLAMAD FORM LE PASAMOS LA ETIQUETA <FORM> Y LUEGO LE DECIMOS CON UNA CONDICION QUE IF THIS.STATE.LOADING ES TRUE FORM = <SPINNER /> Y IMPORTAMOS AXIOS PARA EL AXIOS.POST A FIREBASE

--- burger/src/containers/Checkout/ContactData/ContactData.js
+++ burger/src/containers/Checkout/ContactData/ContactData.js
@@ -3,6 +3,10 @@ import React, { Component } from 'react';
 import Button from '../../../components/UI/Button/Button';
 import classes from './ContactData.css';
 
+import axios from '../../../axios-orders';
+
+import Spinner from '../../../components/UI/Spinner/Spinner';
+
 class ContactData extends Component {
 
     state={
@@ -11,20 +15,67 @@ class ContactData extends Component {
         address: {
             street: '',
             postalCode: ''
+        },
+        loading: false
+    }
+    /* Cuando haga click en el boton Order envie el pedido al servidor. */
+    orderHandler = (event) => {
+
+        event.preventDefault();
+
+         //Declaramos la const order para crear una estructura con la informacion que tendra en la base de datos para las ordenes.
+        this.setState( {loading: true} );
+        const order = {
+            ingredients: this.props.ingredients,
+            price: this.props.price, /* viene del archivo Checkout */
+            customer: {
+                name: 'M.J',
+
+                address: {
+                    street: 'avenida vickuna mackena 2585',
+                    zipCode: '2585',
+                    country: 'Santiago'
+                },
+
+                email: 'marita@test.com'
+            },
+
+            deliveryMethod: 'fastest'
         }
+        axios.post('/orders.json', order)//para agregarles las ordenes a firebase, le pasamos como parametros la const order.
+        .then(response => {
+            this.setState({loading: false});
+            this.props.history.push('/');/*  Para que luego que le click al boton order de la ruta /checkout/Contact-data se redireccione a home */
+
+        })
+        .catch(error => {
+            this.setState({loading: false});
+
+        });
+       
+
+
     }
 
     render (){
+        /* Para el spinner */
+        let form = (
+            <form>
+                <input className={classes.Input}  type="text" name="name" placeholder="Your Name" />
+                <input className={classes.Input} type="email" name="email" placeholder="Your Mail" />
+                <input className={classes.Input} type="text" name="street" placeholder="Street" />
+                <input className={classes.Input} type="text" name="postal" placeholder="Postal Code" />
+                <Button btnType='Success' clicked={this.orderHandler}>Order</Button>
+            </form>
+        );
+        if (this.state.loading) {
+            form = <Spinner />;
+        }
         return (
             <div className={classes.ContactData}>
                 <h4>Enter your Contact Data</h4>
-                <form>
-                    <input className={classes.Input}  type="text" name="name" placeholder="Your Name" />
-                    <input className={classes.Input} type="email" name="email" placeholder="Your Mail" />
-                    <input className={classes.Input} type="text" name="street" placeholder="Street" />
-                    <input className={classes.Input} type="text" name="postal" placeholder="Postal Code" />
-                    <Button btnType='Success'>Order</Button>
-                </form>
+                {form}
+               
             </div>
         );
     }
