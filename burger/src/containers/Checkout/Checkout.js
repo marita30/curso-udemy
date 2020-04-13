0d37956ec656660dee5e24fa1a3c9eede25f47d1
marita30 <marita.jose.ch@gmail.com>
2020-04-13 13:52:37 -0400

    Creamos el nuevo metodo que se llama componentDidMount que primero declaramos una const llamada query que tendra todos los parametros que tiene la variable search que viene del rchivo burgerBilder, luego declaramos otra const llamada ingredients con un objeto {} vacio, luego un ciclo for (let param of query.entries()) que significa que params sera nuestro puntero y que solo entrara los parametros que tiene la variable query, luego ingredients[param[0]] = +param[1] que significa que ingredient={} y param[0] a la llaves que es salad, bacon, meat , etc y param[1] al valor de cada una de las llaves, y se veria asi {salad, '1' , 'bacon', '2' etc y luego un this.setState({ingredients: ingredients}) que a la variable ingredients le pasaremos lo que tiene ingredients{}

--- burger/src/containers/Checkout/Checkout.js
+++ burger/src/containers/Checkout/Checkout.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
+import BurgerBuilder from '../BurgerBuilder/BurgerBuilder';
 
 class Checkout extends Component {
 
@@ -12,14 +13,31 @@ class Checkout extends Component {
         }
     }
 
+    componentDidMount () {
+        const query = new URLSearchParams(this.props.location.search); /* le decimos que busque la locacion de search que esta en el BurgerBuilder.js que incluye el signo de interrogacion, etc */
+        /* Declaramos ingredients con un object vacio */
+        const ingredients = {};
+ /*        luego hacemo un for que os parametros que tiene search que ahora los tiene query entren y se vayan ordenando conforme 
+        ingredients[param[0]] = +params[1]; que seria ingredients{} params[0] seria salad y +param[1] seria 0 , {"salad:0"} y asi sucesivamente. */
+        for (let param of query.entries()){
+            //['salad', '1']
+            ingredients[param[0]] = +param[1];
+        }
 
+        this.setState({ingredients: ingredients});
+    }
+
+ 
+
+
+    /* Para el boton cancell e la ruta /checkout*/
     checkoutCancelledHandler = () => {
 
         this.props.history.goBack();/*  esto simplemente regresa a la ultima pagina osea al home donde cremos la hamburguesa */
 
     }
 
-
+    /* Para el boton continue de la ruta /checkout */
     checkoutContinuedHandler = () => {
 
         this.props.history.replace('/checkout/contact-data');/*  aqui decimos que reemplace la ruta actual con una nueva ruta  */
