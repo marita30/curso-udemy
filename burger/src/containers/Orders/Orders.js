a26008cecb4d8c497babf1bd3b03066097ada5b7
marita30 <marita.jose.ch@gmail.com>
2020-04-14 15:31:37 -0400

    delaramos un state con las variables orders:[] y loading:true, luego el gancho de vida componentDidMount, para hacer una aticion a la base de datos axios.get('/orders.json') y response declaramos una const llamada fetcheOrders = [] y luego un ciclo que let key sera los id de cada orden y ...res.data[key] va a traer todo el object que tiene ese id que sra la orden y asi sucesivamente, importamos axios para que funcione y tambien importamos withErrorHandler para envolver todo el archivo en expor default---- fetching Orders // trayendo informacion desde el backend para mandar a imprimir las orden en la ruta /orders

--- burger/src/containers/Orders/Orders.js
+++ burger/src/containers/Orders/Orders.js
@@ -2,14 +2,48 @@ import React, { Component } from 'react';
 
 import Order from '../../components/Order/Order';
 
+import axios from '../../axios-orders';
+
+import withErrorHanlder from '../../hoc/withErrorHandler/withErrorHandler';
+
 class Orders extends Component {
+
+    state = {
+        orders: [],
+        loading: true
+    }
+
+    componentDidMount () {
+
+        axios.get('/orders.json')
+            .then(res => {
+                const fetcheOrders = [];
+                /* me va a traer todas las ordenes que estan en el backend en firebase, key hace refencia a los id de las ordenes */
+                for (let key in res.data){
+
+                    fetcheOrders.push({
+                        ...res.data[key],
+                        id: key
+                    });
+
+                }
+
+                this.setState({loading: false, orders: fetcheOrders});
+
+            })
+            .catch(error => {
+
+                this.setState({loading: false});
+
+            });
+
+    }
 
     render (){
         return (
 
             <div>
-                <Order />
-                <Order />
+                
             </div>
 
         );
@@ -18,4 +52,4 @@ class Orders extends Component {
 }
 
 
-export default Orders;
+export default withErrorHanlder(Orders, axios);
