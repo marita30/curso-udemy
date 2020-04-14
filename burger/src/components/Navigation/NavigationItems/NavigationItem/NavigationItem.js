f7c99ccc8310ba665c713fa9693ab7dce82f3b32
marita30 <marita.jose.ch@gmail.com>
2020-04-14 14:42:47 -0400

    Importando en el archivo NavigationItem.js NavLink y sustituirlo en la etiqueta <a> sustituir href por to y agregar el props.exact y el activeClassName, luego en el archivo NavigationItems.js cambbiamos la ultima etiqueta por link='/orders' que ahora al entrar a la aplicacion vemos que el boton que se llama checkout ahora se llama orders, y eso te redireccio /orders que /orders contiene o renderiza al componente Orders.js y eso return <order> que order contiene la pagina de ordenes

--- burger/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ burger/src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,14 +1,17 @@
  import React from 'react';
 import classes from './NavigationItem.css';
 
+import { NavLink } from 'react-router-dom';
+
 
  const navigationItem = (props) => (
      
    <li className={classes.NavigationItem}>
-      <a 
-         href={props.link}
-         className={props.active ? classes.active : null }>{props.children} {/* si props.active es true que pasa a classes.active de lo contrario null */}
-      </a>
+      <NavLink
+         to={props.link} /* viene del archivo NavigationItems.js */
+         exact={props.exact} /* viene del archivo NavigationItems.js */
+         activeClassName={classes.active}>{props.children} {/* si props.active es true que pasa a classes.active de lo contrario null */}
+      </NavLink>
    </li>
 );
 
