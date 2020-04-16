37c92c86ffec34c3ef7ae407bab287aac56b8061
marita30 <marita.jose.ch@gmail.com>
2020-04-16 14:43:25 -0400

    configuramos el codigo para transformar los ingredientes, declaramos una const ingredients que va ser igual a un arreglo vacio [], despues un bucle for (let ingredients in props.ingredients) luego dentro del bucle pasamos la const ingredients.push y le pasamos como parametros name: ingredientName que es el punetero let que declaramos enel for y amount: props.ingredients[ingredientName] que es para almacenar la cantidad de los ingredientes, 1,2,3 y asi sucesivamente, luego para dar salidas a los ingredientes declaramos una const ingredientOutput = ingredients.map(ig y eso va retornar un style y un key, y luego en el retrn de la class sustituimos ingredients: {ingredientsOutput} que es la configuracion de la salids y price: que utilizamos Number.parseFloat para que convierta el string en numero para que pueda imprimir solo con dos decimales el precio del props.price que viene del archivo Orders.js

--- burger/src/components/Order/Order.js
+++ burger/src/components/Order/Order.js
@@ -1,13 +1,53 @@
 import React from 'react';
 
 import classes from './Order.css';
+import Orders from '../../containers/Orders/Orders';
 
-const order = (props) => (
-    <div className={classes.Order}>
-        <p>Ingredients: salad(1)</p>
-        <p>Price: <strong>USD 5.45</strong></p>
-    </div>
-);
+const order = (props) => {
+
+    /* Tranformar los ingredientes */
+    const ingredients = [];
+
+    for (let ingredientName in props.ingredients ){
+
+        ingredients.push(
+            { 
+                name: ingredientName,
+                amount: props.ingredients[ingredientName] /* amount para almacenar la cantidad */
+            }
+        ); /* la llave ingredientName hace referencia a la llave que seria los ingredients , bacon , cheese etc y props.ingredients[ingredientName] hace referencia al valor osea la cantidad ejemplo cheese:1 , bacon:3 */
+    } /* props.ingredients viene del archivo Orders.js */
+
+    /* Dar salidas a los ingredientes */
+    const ingredientOutput = ingredients.map(ig => {
+    return <span 
+        style={{
+                textTransform: 'capitalize',
+                display: 'inline-block',
+                margin: '0 8px ',
+                border: '1px solid #ccc',
+                padding: '5px'}}
+        key={ig.name} >{ig.name} ({ig.amount})</span>;
+    });
+
+    return (
+    
+        <div className={classes.Order}>
+        <p>Ingredients: {ingredientOutput}</p>
+            <p>Price: <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong></p> {/* props.price viene del archivo orders.js // parseFloat es oara transformar todo el string a un numero*/}
+        </div>
+
+    );
+    
+};
+
+/* https://lodash.com/docs/4.17.15 */
+/* letterresult = []
+for (let letters in props.data){
+
+    letterresult.push(props.data[letters][0].b.c)
+
+} */
 
 
 export default order;
