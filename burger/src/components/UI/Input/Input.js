cf30baeb32593d74d520d1c4aef7019c0136983b
marita30 <marita.jose.ch@gmail.com>
2020-04-21 13:43:51 -0400

    Agregando el menu desplegable para el object del cntactData.js deliveryMethod fastest o cheapest, agregamos un nuevo casa llamado select , y luego mapeamos el object props.elementConfig.options.map con un puntero llamado option, luego delcaramos una etiquet <option y le pasamos como parametros la key y el value y luego between las etiquetas <option> mandamos a imprimir option que es el puntero . dislayValue que es el valor , ejemplo: option tendra la llave que sera displayValue y displayValue tendra el valor que sera o fastest o cheapest

--- burger/src/components/UI/Input/Input.js
+++ burger/src/components/UI/Input/Input.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import classes from './Input.css';
+import ContactData from '../../../containers/Checkout/ContactData/ContactData';
 
 const input = ( props) => {
     let inputElement = null;
@@ -17,6 +18,21 @@ const input = ( props) => {
             inputElement = <textarea 
                 className={classes.InputElement} {...props.elementConfig} 
                 value={props.value} />;
+            break;
+        case ('select'):
+            inputElement = (
+                <select 
+
+                    className={classes.InputElement} 
+                    value={props.value}> 
+                    {props.elementConfig.options.map(option => ( /* esto viene del orden del state  del object deliveryMethod del archivo ContactData.js */
+                        <option key={option.value} value={option.value} >
+                            {option.displayValue}
+                        </option>
+                    ))}
+
+                </select>
+            );
             break;
         default: 
             inputElement = <input 
