a497d784cc84cedfe8ee94c751c49fea06fbcd49
marita30 <marita.jose.ch@gmail.com>
2020-04-30 13:30:05 -0400

    conectando react con redux, para eso creamos una nueva carpeta llamada store y dentro de esa carpeta un archivo llamado reducer , hacemos la configuracion basica, y luego en el index.js creamos la store importamos rducer y se lo pasamos en la const store = createStore(reducer);

--- redux--01-start/src/index.js
+++ redux--01-start/src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { createStore } from 'redux';
+import reducer from './store/reducer';
+
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+
+/* create store */
+const store = createStore(reducer);
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
