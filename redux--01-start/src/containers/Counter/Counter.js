fedf568ae5fd95cd54f032827284e1877ad17386
marita30 <marita.jose.ch@gmail.com>
2020-04-30 14:17:12 -0400

    conectando store con react , importamos react-redux {connect}, luego despues del render declaramos ua const llamada mapStateToProps = state y tendra ctr: state.counter y luego envolvemos toda la classes en el export default con lo importado connect(mapStateToProps)(counter)

--- redux--01-start/src/containers/Counter/Counter.js
+++ redux--01-start/src/containers/Counter/Counter.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+/* Conectando el store con react */
+import { connect } from 'react-redux';
+
 
 import CounterControl from '../../components/CounterControl/CounterControl';
 import CounterOutput from '../../components/CounterOutput/CounterOutput';
@@ -28,7 +31,7 @@ class Counter extends Component {
     render () {
         return (
             <div>
-                <CounterOutput value={this.state.counter} />
+                <CounterOutput value={this.props.ctr} />
                 <CounterControl label="Increment" clicked={() => this.counterChangedHandler( 'inc' )} />
                 <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
                 <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
@@ -38,4 +41,12 @@ class Counter extends Component {
     }
 }
 
-export default Counter;
+const mapStateToProps = state => {
+    return {
+
+        ctr: state.counter, /* denme el valor del counter en nuestro global administrado  */
+        add: state.add
+    };
+};
+
+export default connect(mapStateToProps)(Counter);
