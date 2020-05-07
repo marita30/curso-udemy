568194ce3dcadb1be4417bae10a38afa0edecd33
marita30 <marita.jose.ch@gmail.com>
2020-05-07 15:02:37 -0400

    declaramos una matriz vacia llamada results [] en initialState, para luego pasarle un id y value en el case 'STORE_RESULT' con .concat , esto es para el boton de store result del archivo counter.js

--- redux--01-start/src/store/reducer.js
+++ redux--01-start/src/store/reducer.js
@@ -1,28 +1,38 @@
 
 const initialState = {
-    counter:0
+    counter:0,
+    results: []
 }
 
 const reducer  = (state = initialState, action) => {
 
     switch (action.type){
         case 'INCREMENT':
-            return {
-                counter: state.counter + 1
-            }
+            const newState = Object.assign({}, state); /* lo que hace esto es que paso un object vacio, y el antiguo object que queremos copiar como el segundo state. */
+            newState.counter = state.counter + 1
+            return newState;
 
         case 'DECREMENT':
             return {
+                ...state,
                 counter: state.counter - 1
             }
         case 'ADD':
             return {
+                ...state,
                 counter: state.counter + action.value
             }
         case 'SUBTRACT':
             return {
+                ...state,
                 counter: state.counter - action.value
             }   
+        case 'STORE_RESULT':
+            return {
+                ...state,
+                results: state.results.concat({id: new Date(), value: state.counter}) /* utilizamos concat para actualizar la matriz, es casi igual a push , pero lo que hace push es que toca el resultado original en el state original y concat no , entonces le decimos que le pase a la matriz results(state.counter). */
+
+            }
     }
    
 
