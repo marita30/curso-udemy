3de5860ae19f017a3586ded2962c8d1ec88a6f11
marita30 <marita.jose.ch@gmail.com>
2020-05-18 14:17:31 -0400

    Cambiando los nombres de las acciones en el case ahora por la nueva configuracion del archivo actions.js , que seria import * as actionTypes from '' y luego en el case seria actionTypes.DECREMENT , O , ADD, INCREMENT , etc

--- redux--01-start/src/store/reducer.js
+++ redux--01-start/src/store/reducer.js
@@ -1,3 +1,5 @@
+import * as actionTypes from './actions';
+
 
 const initialState = {
     counter:0,
@@ -7,33 +9,35 @@ const initialState = {
 const reducer  = (state = initialState, action) => {
 
     switch (action.type){
-        case 'INCREMENT':
+
+        case actionTypes.INCREMENT:
+            
             const newState = Object.assign({}, state); /* lo que hace esto es que paso un object vacio, y el antiguo object que queremos copiar como el segundo state. */
             newState.counter = state.counter + 1
             return newState;
 
-        case 'DECREMENT':
+        case actionTypes.DECREMENT:
             return {
                 ...state,
                 counter: state.counter - 1
             }
-        case 'ADD':
+        case actionTypes.ADD:
             return {
                 ...state,
                 counter: state.counter + action.value
             }
-        case 'SUBTRACT':
+        case actionTypes.SUBTRACT:
             return {
                 ...state,
                 counter: state.counter - action.value
             }   
-        case 'STORE_RESULT':
+        case actionTypes.STORE_RESULT:
             return {
                 ...state,
                 results: state.results.concat({id: new Date(), value: state.counter}) /* utilizamos concat para actualizar la matriz, es casi igual a push , pero lo que hace push es que toca el resultado original en el state original y concat no , entonces le decimos que le pase a la matriz results(state.counter). */
 
             }
-        case 'DELETE_RESULT':
+        case actionTypes.DELETE_RESULT:
             /* const id = 1;
             /* copia de mi matriz results */
             /* const newArray = [...state.results];
