787b39c5425e51d58e6b0a1fc26f274d0fdf000d
marita30 <marita.jose.ch@gmail.com>
2020-05-19 13:40:27 -0400

    creamos dos nuevos files llamaod counter.js y result.js dentro de la carpeta reducers.js , el counter tiene el case pero sin el sotre_result y delete_result y el result tiene el case de solo esos dos store_result y delete_result , teniendo una vez esro pasamos a configurar el index.js , importando los dos nuevos archivos , con los nombres import counterReducer y el otro import resultReducer y luego declarar una const llamada rooutReducer que la pasamos los dos nuevos nomres del archivo para luego pasarselo como parametro cuando create store

--- redux--01-start/src/index.js
+++ redux--01-start/src/index.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
 // para conectar la store
 import { Provider } from 'react-redux';
 
-import reducer from './store/reducer';
+import counterReducer from './store/reducers/counter';
+import resultReducer from './store/reducers/result';
 
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// Para combinar los reducers
+const rootReducer = combineReducers({
+    ctr: counterReducer,
+    res: resultReducer
+})
+
 
 /* create store */
-const store = createStore(reducer);
+const store = createStore(rootReducer);
 
 
 /* instalando la tienda en la aplicacion React. */
