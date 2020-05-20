e9384e73ae1804e4608763a284c10915ab7eefe5
marita30 <marita.jose.ch@gmail.com>
2020-05-20 16:32:02 -0400

    configurando el reducer, para eso creamos el switch case , con dos case ADD_INGREDIENTS y REMOVE_INGREDIENTS, luego configuramos el primero que es ADD_INGREDIENTS , primero haciendo una copia del state , luego lo configuramos para que los vlores de los ingredientes cambien , para eso declaramos ingredients:{...satet.ingredients, luego de esa copia configuramos [action.ingredientName]: state.ingredients[action.ingredientsName] + 1} esto es que el primer action va a contener las llaves del object ingredients en este caso es bacon, salad, meet, etc, y luego de los : va a contener el valor de esas llaves y se le agregara 1

--- burger/src/store/reducer.js
+++ burger/src/store/reducer.js
@@ -1,12 +1,34 @@
 import * as actionTypes from "./actions";
 
 const initialState = {
-    ingredients: null,
+    ingredients: {
+        salad: 0,
+        bacon: 0,
+        cheese: 0,
+        meat:0
+    },
     totalPrice: 4
 };
 
 const reducer  = (state= initialState, action) => {
 
+    switch (action.type) {
+        case actionTypes.ADD_INGREDIENT:
+            return {
+                ...state,
+                ingredients: {
+                    ...state.ingredients,
+                    [action.ingredientName]: state.ingredients[action.ingredientName] + 1 /* en el primer [action.ingredientName] recibimos la llave de los ingredients bacon, salad, etc y luego de los : recibimos el valor + 1 */
+                }
+
+            };
+        case actionTypes.REMOVE_INGREDIENT:
+            return {
+
+            };
+        default: 
+            return state;
+    }
 };
 
 
