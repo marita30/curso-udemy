66b0c834e6ae81a33e9dabaf6aa857b033577054
marita30 <marita.jose.ch@gmail.com>
2020-05-25 14:00:40 -0400

    QUitamos la const que declaramos antes de la class llamda INGREDIENTS_PRICES , para luego configurarlo de otra manera con eol reducer

--- burger/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -16,14 +16,6 @@ import Spinner from '../../components/UI/Spinner/Spinner';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import axios from '../../axios-orders';
 
-const INGREDIENT_PRICES = {
-    salad: 0.5,
-    cheese: 0.4,
-    meat: 1.3,
-    bacon: 0.7
-};
-
-
 
 class BurgerBuilder extends Component {
 
