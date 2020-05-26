c0b311e58c328ffc2bc4b4c287b882e116066310
marita30 <marita.jose.ch@gmail.com>
2020-05-26 13:36:17 -0400

    Moviendo actions.js a la nueva carpeta store/actions/actions.js

--- redux--01-start/src/store/actions.js
+++
@@ -1,8 +0,0 @@
-
-// EXPORTANDO TODAS LAS ACCIONES.
-export const INCREMENT = 'INCREMENT';
-export const DECREMENT = 'DECREMENT';
-export const ADD = 'ADD';
-export const SUBTRACT = 'SUBTRACT';
-export const STORE_RESULT = 'STORE_RESULT';
-export const DELETE_RESULT = 'DELETE_RESULT';
