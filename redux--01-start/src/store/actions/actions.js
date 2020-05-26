5ac21580d5a4a759c67282a8bac884ceaa4c12ca
marita30 <marita.jose.ch@gmail.com>
2020-05-26 14:13:14 -0400

    Creamos las demas funciones de las acciones

--- redux--01-start/src/store/actions/actions.js
+++ redux--01-start/src/store/actions/actions.js
@@ -9,9 +9,48 @@ export const DELETE_RESULT = 'DELETE_RESULT';
 
 /* Creacion del action create -- es slo una funcion que devuelve una accion o que crea una accion contra el nombre */
 
-const increment = () => {
+export const increment = () => {
     return {
         type: INCREMENT
+
+    };
+};
+
+export const decrement = () => {
+    return {
+        type: DECREMENT
+
+    };
+};
+
+export const add = (valor) => {
+    return {
+        type: ADD,
+        value: valor
+
+    };
+};
+
+export const subtract = (valor) => {
+    return {
+        type: SUBTRACT,
+        value: valor
+
+    };
+};
+
+export const storeResult= (res) => {
+    return {
+        type: STORE_RESULT,
+        result: res 
+
+    };
+};
+
+export const deleteResult= (resElId) => {
+    return {
+        type: DELETE_RESULT,
+        resultElId: resElId
 
     };
 };
