aca92c81571a4a82247d3d6c83865e2a4049dd39
marita30 <marita.jose.ch@gmail.com>
2020-05-28 15:55:47 -0400

    Importamos axios, y todos los actionTypes, luego delcaramos las funciones para cada accion del actionTypes para el contactData, purchaseBurgerSuccess, purchaseBurgerFail y le agregamos la funcion del dispatch que seria purchaseBurgerStart donde contendra el axios.post la peticion del back end a firebase de las ordenes para que la maneje redux , solo falta conectarlas al contactData,js

---
+++ burger/src/store/actions/order.js
@@ -0,0 +1,37 @@
+import * as actionTypes from "./actionTypes";
+
+import axios from '../../axios-orders';
+
+export const purchaseBurgerSuccess = (id, orderData) => {
+    return {
+        type: actionTypes.PURCHASE_BURGER_SUCCESS,
+        orderId: id,
+        orderData: orderData
+    };
+};
+
+export const purchaseBurgerFail = (error) => {
+    return {
+        type: actionTypes.PURCHASE_BURGER_FAIL,
+        error: error
+
+    };
+};
+
+export const purchaseBurgerStart = (orderData) => {
+    return dispatch => {
+        axios.post('/orders.json', orderData)//para agregarles las ordenes a firebase, le pasamos como parametros la const order.
+        .then(response => {
+            console.log(response.data);
+            dispatch(purchaseBurgerSuccess(response.data, orderData))
+            /* this.props.history.push('/');/*  Para que luego que le click al boton order de la ruta /checkout/Contact-data se redireccione a home */ 
+
+        })
+        .catch(error => {
+            dispatch(purchaseBurgerFail(error));
+          /*   this.setState({loading: false}); */
+
+        });
+    };
+
+};
