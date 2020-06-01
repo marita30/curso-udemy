3c4ed950404edf7a8ac063a47c512d9576e34e85
marita30 <marita.jose.ch@gmail.com>
2020-06-01 13:40:23 -0400

    combinando los reducer , primero importamos burgerBuilder del reducer y lo llamamos burgerBuilderReducer, luego declaramos una const llamada rootReducer = combineReducer ({ burgerBuilder: burgerBuilderReducer, order: orderReducer )} ahi combinas los dos reducers y luego vamos hacer las configuraciones en el BurgerBuilder en el metodo de redux map

--- burger/src/index.js
+++ burger/src/index.js
@@ -13,15 +13,24 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
 /* Creando el store */
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 /* importando reducer.js */
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
+
+/* CombineReducers */
+import orderReducer from './store/reducers/order';
 
 /* thunk  */
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+/* Combine reducer */
+const rootReducer =  combineReducers({
+    burgerBuilder: burgerBuilderReducer,
+    order: orderReducer
+})
+
                                     /* PARA SABER EL ESTADO DE NUESTRA APLICACION CON REDUX DEVTOOLS */
-const store = createStore(burgerBuilderReducer, composeEnhancers( 
+const store = createStore(rootReducer, composeEnhancers( 
     applyMiddleware(thunk)
 ));
 
