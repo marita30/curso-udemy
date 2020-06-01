db91c14744bc0585a8929f0192f9827f8cdd18e9
marita30 <marita.jose.ch@gmail.com>
2020-06-01 17:03:35 -0400

    Ahora vamos a pasar las ordenes para que la maneje redux, en este caso agregamos dos funciones llamadas, ORDERS_GET_SUCESS y ORDERS_GET_FAIL, creamos las funciones para esas dos acciones nuevas que la funcion ordersGetFails return type y error y la otra funcion orderGetSucess return type, orders y por ultimo creamos la funcion del dispatch uqe cntedra axios.get que lo teniamos en el container orders.js, y le cambiamos solo los this.setState por dispatch para luego ir al reducer/order.js

--- burger/src/store/actions/order.js
+++ burger/src/store/actions/order.js
@@ -1,6 +1,8 @@
 import * as actionTypes from "./actionTypes";
 
 import axios from '../../axios-orders';
+import order from "../../components/Order/Order";
+
 
 export const purchaseBurgerSuccess = (id, orderData) => {
     return {
@@ -49,3 +51,50 @@ export const purchaseInit = () => {
         type: actionTypes.PURCHASE_INIT
     };
 };
+
+
+
+/* Orders */
+
+export const ordersGetFails = (error) => {
+    return {
+        type: actionTypes.ORDERS_GET_FAIL,
+        error: error
+
+    };
+};
+
+export const ordersGetSuccess = (orderData) => {
+    return {
+        type: actionTypes.ORDERS_GET_SUCESS,
+        orders: orderData
+    };
+};
+
+export const getOrders = () => {
+    return dispatch => {
+        axios.get('/orders.json')
+            .then(res => {
+                const fetcheOrders = [];
+                /* me va a traer todas las ordenes que estan en el backend en firebase, key hace refencia a los id de las ordenes */
+                for (let key in res.data){
+
+                    fetcheOrders.push({
+                        ...res.data[key],
+                        id: key
+                    });
+
+                }
+
+                dispatch(ordersGetSuccess(fetcheOrders));
+
+            })
+            .catch(error => {
+
+                dispatch(ordersGetFails(error));
+
+            });
+
+    }
+    }
+
