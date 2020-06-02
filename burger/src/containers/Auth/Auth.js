b70925d609d7a8814523e0b5419ce0854916785f
marita30 <marita.jose.ch@gmail.com>
2020-06-02 15:07:58 -0400

    Agregando el athentication, para eso importamos el Input.js y Button.js luego definimos el state que va tener la misma logica del contactData.js copiamos y ponemos email y password , luego en el render copiamos del contactData la const formElementsArray y luego en otra const form = forElementsArray.map  y al final el rturn  le pasamos la variable {form} y la etiqueta <Button>

---
+++ burger/src/containers/Auth/Auth.js
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+
+import Input from '../../components/UI/Input/Input';
+import Button from '../../components/UI/Button/Button';
+
+class Auth extends Component {
+
+    state = {
+        controls: {
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Mail Address',
+                },
+                value: '',
+                /* le decimos que este campo no debe de estar vacio. */
+                validation: {
+                    required: true,
+                    isEmail: true
+                },
+                valid: false,
+                touched: false
+            },
+            password: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'password',
+                    placeholder: 'Your Password',
+                },
+                value: '',
+                /* le decimos que este campo no debe de estar vacio. */
+                validation: {
+                    required: true,
+                    minLength: 7
+                },
+                valid: false,
+                touched: false
+            }
+            
+        }   
+    }
+
+    render () {
+         /* Para el nuevo state con muchos object */
+         const formElementsArray= [];
+         for (let key in this.state.controls){ /* Key es el identificador de las llaves de los object que estan en el this.state, name, street, zip code etc. */
+ 
+             formElementsArray.push({
+                 id: key,
+                 config: this.state.controls[key]
+             });
+         }
+
+         const form = formElementsArray.map( formElement => (
+             <Input 
+                key={formElement.id}
+                elementType={formElement.config.elementType} 
+                elementConfig={formElement.config.elementConfig}
+                defaultValue={formElement.config.value}
+                valid={formElement.config.valid} /* para dar css en el archivo input que muestre de un color los input cuando esten false y cuando esten true de otro color */
+                shouldValidate= {formElement.config.validation} /* Para saber que el ultimo object no tiene regla porque es un menu desplegable. */
+                touched = {formElement.config.touched}
+                changed={(event) => this.inputChangedHandler(event, formElement.id)}
+            />
+
+         ));
+
+        return (
+            <div>
+                <form>
+                    {form}
+                    <Button btnType="Success" > SUBMIT  </Button>
+                    
+                </form>
+            </div>
+
+        );
+    }
+    
+}
+
+export default Auth;
