78335ad678a74ba27e5386813f06b5b3e373ab17
marita30 <marita.jose.ch@gmail.com>
2020-06-02 15:08:36 -0400

    Poniendo un console.log para hacer unas pruebas

--- burger/src/components/UI/Button/Button.js
+++ burger/src/components/UI/Button/Button.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import classes from './Button.css';
 
-const button = (props) => (
+const button = (props) => {
+    console.log(props)
+    return (
+            <button
+            disabled={props.disabled} /* viene del archivo contactData.js */
+            className={[classes.Button, classes[props.btnType]].join('')} /* string */
+            onClick={props.clicked} /* viene del aechivo orderSummary */
+        >{props.children}</button>
+        
 
-    <button
-        disabled={props.disabled} /* viene del archivo contactData.js */
-        className={[classes.Button, classes[props.btnType]].join('')} /* string */
-        onClick={props.clicked} /* viene del aechivo orderSummary */
-    >{props.children}</button>
+    )
     
-); /* donde devuelvo jsx ()*/
+}; /* donde devuelvo jsx ()*/
 
 
 export default button;
