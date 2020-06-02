a6c1b8e9424939e092088004e0c7e53c6c6a6b33
marita30 <marita.jose.ch@gmail.com>
2020-06-02 15:53:38 -0400

    Agregando el metodo inputChangedHandler para actualizar el estado ya cuando el usuario ingrese algo en los input y tambien validar las reglas que tiene que cumplir, por ejemplo en el paswordd tiene que ser minimo 7 digitos

--- burger/src/containers/Auth/Auth.js
+++ burger/src/containers/Auth/Auth.js
@@ -72,7 +72,18 @@ class Auth extends Component {
 
     }
 
-    
+    inputChangedHandler = (event, controlName) => {
+        const updatedControls = {
+            ...this.state.controls,
+            [controlName]: {
+                ...this.state.controls[controlName],
+                value: event.target.value,
+                valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
+                touched: true
+            }
+        };
+        this.setState({controls: updatedControls});
+    }
 
 
     render () {
