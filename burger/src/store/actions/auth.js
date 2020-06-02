b46da74d9ac7aad97d71c3d55d5f916034043720
marita30 <marita.jose.ch@gmail.com>
2020-06-02 17:08:33 -0400

    Agregando el token, para eso nos vamos a la funcion dispatch auth , declaramos una const llamada authData que va a contener el email, password y returnSecureToken luego de eso le hacemos el axios.post y le pasamos una url que la vamos a encontrar en chrome buscanso firebase rest auth , y cambiamos la key por la de nosotros de firebase, luego hacemos el response y el catch por si tenemos un error y en firebase habilitamos el metodo de authenticate con correo y pasword y listo

--- burger/src/store/actions/auth.js
+++ burger/src/store/actions/auth.js
@@ -27,6 +27,20 @@ export const authFail = (error) => {
 export const auth = (email, password) => {
     return dispatch => {
         dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        }
+        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBJGbf9wGCpGCwcG9bGkqHhaqOocoTQTds', authData)
+        .then(response => {
+            console.log(response);
+            dispatch(authSucess(response.data));
+        })
+        .catch(error => {
+            console.log(error);
+            dispatch(authFail(error));
+        });
 
     };
 };
