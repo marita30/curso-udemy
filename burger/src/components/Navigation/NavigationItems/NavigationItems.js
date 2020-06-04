d189a2e7bbf2895f4bedd8e17c6b6065a20148bd
marita30 <marita.jose.ch@gmail.com>
2020-06-04 14:48:09 -0400

    Configuramos para que si el usuario no  este authenticated que no se le muestre el boton de orders de lo contrario si esta uthenticado que le muestre el boton orders

--- burger/src/components/Navigation/NavigationItems/NavigationItems.js
+++ burger/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -5,7 +5,15 @@ import NavigationItem from './NavigationItem/NavigationItem';
 const navigationItems = (props) => (
     <ul className={classes.NavigationItems}> 
         <NavigationItem link="/" exact>Burger Builder</NavigationItem>
-        <NavigationItem link="/orders">Orders</NavigationItem>
+
+
+        {/* Si isAuthenticated es verdadero que se le muestre el boton de orders de lo contrario que no lo muestre  */}
+        { props.isAuthenticated
+        ? <NavigationItem link="/orders">Orders</NavigationItem>
+        : null }
+
+
+        {/* Si esta authenticado o no  */}
         { !props.isAuthenticated
         ? <NavigationItem link="/auth"> Authentication </NavigationItem>
         : <NavigationItem link="/logout"> Logout </NavigationItem> }
