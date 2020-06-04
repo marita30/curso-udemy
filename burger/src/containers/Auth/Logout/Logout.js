9694d1a6acaab1c379272fc1d51e98c1cad9ed91
marita30 <marita.jose.ch@gmail.com>
2020-06-04 14:31:25 -0400

    Creamos un nuevo contenedor para el redireccionamiento del Logout , importamos redux para connect, importamos todo los actions from index, luego dentro de laa clase Logout creamo el componentDidMount que se le va a pasar la variable que vamos a definir en el metodo redux mapDoispatchToProps que seria onLogout que dispatch(actions.logout que logout lo tnemos en actions/auth, y luego importamos Redirect form react-router-dom para que dentro del render return <Redirect to=/

---
+++ burger/src/containers/Auth/Logout/Logout.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+
+/* Redux */
+import { connect } from 'react-redux';
+
+/* Actions */
+import * as actions from '../../../store/actions/index';
+
+class Logout extends Component {
+
+    componentDidMount () {
+        this.props.onLogout();
+    }
+
+    render(){
+        return <Redirect to="/"/>;
+    }
+} 
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onLogout: () => dispatch(actions.logout())
+    };
+};
+
+export default connect(null, mapDispatchToProps)(Logout);
