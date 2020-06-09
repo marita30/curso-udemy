1c570a895031ce95991a5256bdd4053e5946434a
marita30 <marita.jose.ch@gmail.com>
2020-06-09 13:19:25 -0400

    Solucionando errores , agregamos la route auth en routes= ( <switch>

--- burger/src/App.js
+++ burger/src/App.js
@@ -36,6 +36,7 @@ class App extends Component{
         <Switch>
             {/* Uthenticate */}
             <Route path="/logout" component={Logout} />
+            <Route path="/auth" component={Auth} />
             <Route path="/Checkout" component={Checkout} />
             <Route path="/orders" component={Orders} /> {/* el boton para que se direccione aqui esta en el archivo NavigationItems.js */}
             <Route path="/" exact component={BurgerBuilder} />
