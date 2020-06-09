34e02d4488de0aee1676603d24cf4ce5ca8cbcd7
marita30 <marita.jose.ch@gmail.com>
2020-06-09 15:44:45 -0400

    instalamos npm run build , que esto construira nuestro proyecto similar al inicio de npm .

---
+++ burger/build/precache-manifest.753764a1558d6ab5ee14223c48e7cb40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29ac3cd28249504edb22977b327b7b48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8c746aa63f903ad0044",
+    "url": "/static/css/main.40871f74.chunk.css"
+  },
+  {
+    "revision": "a242b9f76b4e1d81f703",
+    "url": "/static/js/2.ceacd959.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/static/js/2.ceacd959.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8c746aa63f903ad0044",
+    "url": "/static/js/main.8ed43413.chunk.js"
+  },
+  {
+    "revision": "735fbabcb5c54bf45458",
+    "url": "/static/js/runtime-main.9018ea8a.js"
+  },
+  {
+    "revision": "8d958e9f2d8da2915bee89672630123c",
+    "url": "/static/media/burger.8d958e9f.png"
+  }
+]);
