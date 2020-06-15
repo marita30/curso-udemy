7235b2960cba571c70e851c57fef8c3162d21fde
marita30 <marita.jose.ch@gmail.com>
2020-06-15 13:34:10 -0400

    AGregando a los input en el evento onChange en el input title tambien le agregamos el amount y en el input amount tambien le agregamos title para que no se pierda los cambios y asi quitemos el error que nos daba en la console

--- hooks-01-starting-project/src/components/Ingredients/IngredientForm.js
+++ hooks-01-starting-project/src/components/Ingredients/IngredientForm.js
@@ -20,12 +20,12 @@ const IngredientForm = React.memo(props => {
           <div className="form-control">
             <label htmlFor="title">Name</label>
                                                                                                   {/* ACtualiza title con los valores  target.value que son los que son ingresados por el usuario. */}
-            <input type="text" id="title" value={inputState[0].title} onChange={event => inputState[1]({title: event.target.value})} />
+            <input type="text" id="title" value={inputState[0].title} onChange={event => inputState[1]({title: event.target.value, amount: inputState[0].amount})} />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
                                                                                                         {/* ACtualiza amount con los valores target.value que son los que son ingresados por el usuario. */}
-            <input type="number" id="amount" value={inputState[0].amount} onChange={event => inputState[1]({amount: event.target.value})} />
+            <input type="number" id="amount" value={inputState[0].amount} onChange={event => inputState[1]({amount: event.target.value, title: inputState[0].title})} />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
