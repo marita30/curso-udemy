3e3e9145e6f9d3e15151b5afa616df9edae484e8
marita30 <marita.jose.ch@gmail.com>
2020-06-15 15:45:17 -0400

    Ahora trabajaremos en el boton, para eso declaramos el useState asi const [userIngredients, setUserIngredients] = useState([]) y lo declaramos con un arreglo porque es una lista de ingredientes, luego agregamos el component addIngredientsHandler que contendra los ingredient y declaramos dentro el segundo parametro de actualizar los ingredientes que tienen useState en este caso seria setUserIngredients(prevIngredients => [...prevIngredients, {id: Math.Random().toString(), ...ingredient} le decimos que le pasamos el estado prevIngredients y le pasamos los elemento que tienen que ir id , ...Ingredient y todo lo que tiene  la copia Ingredient que seria title y amount , luego en el return en la etiqueta IngredientForm le pasamos el nuevo componente addIngredientHandler dentro de una variable llamada onAddIngredient y luego debajo de la etiqueta <Search/> declaramos la etiqueta <IngredientList que tendra ingredients = {userIngredients} que es la variable que delcaramos en el useState y el onRemoveItem que viene del archivo INgredientList y por ultimo importamos el IngredientList y le pasamos el onAddIngredients al archivo Ingredients.js

--- hooks-01-starting-project/src/components/Ingredients/Ingredients.js
+++ hooks-01-starting-project/src/components/Ingredients/Ingredients.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
+
 import Search from './Search';
 
-function Ingredients() {
+const Ingredients = () =>  {
+  /* ingredients es para obtener los userIngredientes y setUserIngredients es para actualizar la matriz que esta en useState cuando el usuario ingrese nuevos ingredientes.s */
+  const [userIngredients, setUserIngredients] = useState([]); 
+
+
+  /* Agregar ingredients */
+
+  const addIngredientHandler = ingredient => {
+                                          /* es un operadr de propagacion que toma todos los elementos de nuestra matriz anterior y lo agrega como elementos a esta nueva matriz */
+    setUserIngredients(prevIngredients => [...prevIngredients,
+         {id: Math.random().toString(), ...ingredient}
+        ]);
+  };
+
+
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient = {addIngredientHandler} />
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+          <IngredientList ingredients = {userIngredients} onRemoveItem = { () => {}}/>
       </section>
     </div>
   );
