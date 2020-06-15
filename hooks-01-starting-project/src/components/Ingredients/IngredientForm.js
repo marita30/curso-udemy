2f38f838000763f59af71fb34b1a9b1736b8c0ac
marita30 <marita.jose.ch@gmail.com>
2020-06-15 13:15:14 -0400

    Utiizando un gancho llamado useState qu ehace que el objeto que le declares se vuelva una matriz y lo utilizamos para cambiar los valores de los input de title y amount, despues nos vamos a cada uno de los input y le decimos value= {inputState[0].title} el inputState es la variable donde se esta guardando la nueva matris del useState y como es una matriz le decimos en el ubIndcie 0. title la primera llave y despues le pasamos el event onChange para que tome el nuevo valor que el usuario ingreso en ese input y asi mismo con el otro input de amount, despues nos vamos a la aplicacion y obtenemos un warning que lo vamos a componer en el proximo commit

--- hooks-01-starting-project/src/components/Ingredients/IngredientForm.js
+++ hooks-01-starting-project/src/components/Ingredients/IngredientForm.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card';
 import './IngredientForm.css';
 
 const IngredientForm = React.memo(props => {
+  /* Esto ahora lo pasa como un arreglo */
+ const inputState = useState({title: '', amount: ''});
+
+
   const submitHandler = event => {
     event.preventDefault();
     // ...
@@ -15,11 +19,13 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" />
+                                                                                                  {/* ACtualiza title con los valores  target.value que son los que son ingresados por el usuario. */}
+            <input type="text" id="title" value={inputState[0].title} onChange={event => inputState[1]({title: event.target.value})} />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" />
+                                                                                                        {/* ACtualiza amount con los valores target.value que son los que son ingresados por el usuario. */}
+            <input type="number" id="amount" value={inputState[0].amount} onChange={event => inputState[1]({amount: event.target.value})} />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
