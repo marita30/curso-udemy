5af55aba1408f2d0735a171e33bed78f7c4af8ba
marita30 <marita.jose.ch@gmail.com>
2020-06-16 16:03:49 -0400

    agregando useRef, lo que estamos haciendo aqui es que agregamos el setTimeout para ver cuando el usuario pause , si el usuario pause se manda la solicitud de lo contrario no, y lo utilizamos dentro del useEffect

--- hooks-01-starting-project/src/components/Ingredients/Search.js
+++ hooks-01-starting-project/src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
@@ -8,30 +8,39 @@ const Search = React.memo(props => {
   const { onLoadIngredients } = props;
   /* El enteredFilter es el que obtiene el valor y setEnteredFilter es el que cambia el valor.*/
   const [enteredFilter, setEnteredFilter] = useState('');
+  const inputRef = useRef('');
 
   /* SE EJECUTARA CUANDO EL FILTRO INGRESADO CAMBIE */
   useEffect(() => {
 
-    /* Si la longitud del filtro ingresada es igual a cero entonces mi periodo de consulta no sera nada. `?` interpolacion de cadenas para inyectar valores dinamicos. */
-    const query = 
-      enteredFilter.length === 0 
-      ? '' 
-      : `?orderBy="title"&equalTo="${enteredFilter}"`;
+    /* Si el usuario pause la escritura durante 500 milisegundos y solo si esto sucedio quiero enviar la siguien solicitud. */
+    setTimeout(() => {
+      if (enteredFilter === inputRef.current.value) {
+          /* Si la longitud del filtro ingresada es igual a cero entonces mi periodo de consulta no sera nada. `?` interpolacion de cadenas para inyectar valores dinamicos. */
+        const query = 
+        enteredFilter.length === 0 
+        ? '' 
+        : `?orderBy="title"&equalTo="${enteredFilter}"`;
 
-    fetch('https://react-hooks-562ed.firebaseio.com/ingredients.json' + query).then(
-          response => response.json()
-      ).then(responseData => {
-          const loadedIngredients = [];
-          for (const key in responseData) {
-            loadedIngredients.push({
-              id: key,
-              title: responseData[key].title,
-              amount: responseData[key].amount
-            });
-          }
-          onLoadIngredients(loadedIngredients) /* viene del archivo Ingredients.js */
-      });
-  }, [enteredFilter, onLoadIngredients]);
+      fetch('https://react-hooks-562ed.firebaseio.com/ingredients.json' + query).then(
+            response => response.json()
+        ).then(responseData => {
+            const loadedIngredients = [];
+            for (const key in responseData) {
+              loadedIngredients.push({
+                id: key,
+                title: responseData[key].title,
+                amount: responseData[key].amount
+              });
+            }
+            onLoadIngredients(loadedIngredients) /* viene del archivo Ingredients.js */
+        });
+
+      }
+     
+    }, 500);
+
+  }, [enteredFilter, onLoadIngredients, inputRef]);
 
   return (
     <section className="search">
@@ -39,6 +48,7 @@ const Search = React.memo(props => {
         <div className="search-input">
           <label>Filter by Title</label>
           <input 
+            ref={inputRef}
             type="text" 
             value={enteredFilter} 
             onChange={event => setEnteredFilter(event.target.value)} 
