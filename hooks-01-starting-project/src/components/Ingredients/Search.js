c293106f8448b502f28edd81c9ea4d2141578177
marita30 <marita.jose.ch@gmail.com>
2020-06-16 14:03:19 -0400

    Configurando el filter by, ahora usamos useEffect para que se ejecute cada vez que el filtro cambie, dentro de esa funcion useEffect hacemos los mimso pasos que hicimos en el useEffect de archivo Ingredients.js , lo unico que antes declaramos const query que tendra un filtro que nos permite firebase y se la agregamos a la url de la peticion

--- hooks-01-starting-project/src/components/Ingredients/Search.js
+++ hooks-01-starting-project/src/components/Ingredients/Search.js
@@ -4,9 +4,34 @@ import Card from '../UI/Card';
 import './Search.css';
 
 const Search = React.memo(props => {
+
+  const { onLoadIngredients } = props;
   /* El enteredFilter es el que obtiene el valor y setEnteredFilter es el que cambia el valor.*/
   const [enteredFilter, setEnteredFilter] = useState('');
 
+  /* SE EJECUTARA CUANDO EL FILTRO INGRESADO CAMBIE */
+  useEffect(() => {
+
+    /* Si la longitud del filtro ingresada es igual a cero entonces mi periodo de consulta no sera nada. `?` interpolacion de cadenas para inyectar valores dinamicos. */
+    const query = 
+      enteredFilter.length === 0 
+      ? '' 
+      : `?orderBy="title"&equalTo=""${enteredFilter}`;
+
+    fetch('https://react-hooks-562ed.firebaseio.com/ingredients.json' + query).then(
+          response => response.json()
+      ).then(responseData => {
+          const loadedIngredients = [];
+          for (const key in responseData) {
+            loadedIngredients.push({
+              id: key,
+              title: responseData[key].title,
+              amount: responseData[key].amount
+            });
+          }
+          onLoadIngredients(loadedIngredients) /* viene del archivo Ingredients.js */
+      });
+  }, [enteredFilter, onLoadIngredients]);
 
   return (
     <section className="search">
